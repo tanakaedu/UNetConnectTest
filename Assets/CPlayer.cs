a869338df199b841b277db961b0e1f443d8a93b9
YuTanaka <YRK00337@nifty.com>
2015-08-27 18:17:52 +0900

    操作の実装

--- Assets/CPlayer.cs
+++ Assets/CPlayer.cs
@@ -1,15 +1,23 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.Networking;
 
-public class CPlayer : MonoBehaviour {
+public class CPlayer : NetworkBehaviour {
+    public float SPEED = 5.0f;
+    private Rigidbody rig = null;
 
 	// Use this for initialization
 	void Start () {
-	
+        rig = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        if (!isLocalPlayer) return;
+
+        Vector3 move = Vector3.zero;
+        move.x = Input.GetAxisRaw("Horizontal");
+        move.y = Input.GetAxisRaw("Vertical");
+        rig.velocity = move * SPEED;
 	}
 }
